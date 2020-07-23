56053ee88129753ccfea5c7c70c9b00f488debd9
Mitchell Moore <mitchell2077@gmail.com>
2020-07-22 21:31:55 -0300

    add email for interview

--- src/main/java/com/app/mystore/service/InterviewService.java
+++ src/main/java/com/app/mystore/service/InterviewService.java
@@ -1,8 +1,12 @@
 package com.app.mystore.service;
 
+import com.app.mystore.dao.ApplicationDao;
+import com.app.mystore.dao.ApplicationDaoImpl;
 import com.app.mystore.dao.InterviewDao;
+import com.app.mystore.dto.Application;
 import com.app.mystore.dto.Interview;
 import com.app.mystore.dto.JobPost;
+import com.app.mystore.utils.MystoreHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +15,25 @@ public class InterviewService {
     @Autowired
     public InterviewDao dao;
 
+    @Autowired
+    public ApplicationDaoImpl applicationDao;
+
+    @Autowired
+    public MystoreHelper helper;
+
     public Interview fetchByInterviewID(int interviewID) {
         Interview interview = dao.getByInterviewID(interviewID);
         return interview;
     }
 
     public Boolean addInterview(Interview interview) {
+        if(interview.getNotify().equals("true")){
+            Application application = applicationDao.getByApplicationID(interview.getApplicationID());
+            if(application != null){
+                String body = "Hello,\n\n Thank you for your interest for working at myStore. We would like to set up an interview via: "+ interview.getType()+" , on " + interview.getDate() + " , at " + interview.getTime();
+                helper.sendEmail(application.getEmail(), body, "Interview for myStore");
+            }
+        }
         int result = dao.insertInterview(interview);
         if(result > 0){
             return true;
