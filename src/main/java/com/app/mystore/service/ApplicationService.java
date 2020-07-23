931ad81bd92cda1c1b98e3d3c1eeb2a34256d9f7
Mitchell Moore <mitchell2077@gmail.com>
2020-07-23 18:19:29 -0300

    add notification to manager

--- src/main/java/com/app/mystore/service/ApplicationService.java
+++ src/main/java/com/app/mystore/service/ApplicationService.java
@@ -1,7 +1,10 @@
 package com.app.mystore.service;
 
 import com.app.mystore.dao.ApplicationDao;
+import com.app.mystore.dao.NotificationsDao;
 import com.app.mystore.dto.Application;
+import com.app.mystore.dto.Notification;
+import com.app.mystore.enums.NotificationTypeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,6 +14,9 @@ public class ApplicationService {
 
     @Autowired
     public ApplicationDao dao;
+
+    @Autowired
+    private NotificationsDao notificationDao;
 
     public List<Application> fetchAll() {
         List<Application> applications = dao.fetchAll();
@@ -40,6 +46,20 @@ public class ApplicationService {
     public Boolean addApplication(Application newApplication) {
         int result = dao.insertApplication(newApplication);
         if(result > 0){
+            int managerUserID = 6;
+            String applicationNotification = "New application from " + newApplication.getFirstName();
+            Notification notification = new Notification();
+            NotificationTypeEnum notificationTypeEnum = NotificationTypeEnum.JOB_NOTIFICATION;
+            notification.setUserId(managerUserID);
+            notification.setNotification(applicationNotification);
+            notification.setNotificationType(notificationTypeEnum.getType());
+            try{
+                notificationDao.createNotification(notification);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
             return true;
         }
         else{
