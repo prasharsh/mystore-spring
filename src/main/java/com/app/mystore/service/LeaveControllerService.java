3e0a5f683b406903301842e12e7c34c5d9768c42
Khanjika <kh779923@dal.ca>
2020-07-25 14:18:50 -0300

    Notification to manager and employee
    Signed-off-by: Khanjika <kh779923@dal.ca>

--- src/main/java/com/app/mystore/service/LeaveControllerService.java
+++ src/main/java/com/app/mystore/service/LeaveControllerService.java
@@ -6,16 +6,39 @@ import org.springframework.stereotype.Service;
 import com.app.mystore.dao.LeaveDao;
 import com.app.mystore.dao.UserDao;
 import com.app.mystore.dto.Leave;
+import com.app.mystore.dto.Notification;
+import com.app.mystore.dto.User;
 
 @Service("leaveControllerService")
 public class LeaveControllerService {
 	
 	@Autowired
 	public LeaveDao leaveDao;
+	
+	@Autowired
+	public NotificationService ns;
+	
+	@Autowired
+	public UserDao dao;
 
 	public String applyLeave(Leave leaveDetails, int empid) {
 		// TODO Auto-generated method stub
 		String result = leaveDao.enterLeaveData(leaveDetails, empid);
+		if (result== "Success")
+		{ 
+		Notification notification= new Notification();
+		
+		try {
+			User user = dao.getUseridById(empid+"");
+			notification.setUserId(dao.getManagerId());
+			notification.setNotification(user.getFirstName()+" "+user.getLastName()+" has applied for leave.");
+			notification.setNotificationType("Leave Notification");
+			ns.createNotification(notification);	
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
 		return result;
 	}
 
@@ -33,11 +56,41 @@ public class LeaveControllerService {
 
 	public String acceptLeave(Leave leave, int empid) {
 		String result=leaveDao.acceptLeave(leave, empid);
+		if (result== "Success")
+		{ 
+		Notification notification= new Notification();
+		
+		try {
+			User user = dao.getUseridById(empid+"");
+			notification.setUserId(empid);
+			notification.setNotification("Leave accepted");
+			notification.setNotificationType("Leave Notification");
+			ns.createNotification(notification);	
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
 		return result;
 	}
 
 	public String rejectLeave(Leave leave, int empid) {
 		String result=leaveDao.rejectLeave(leave, empid);
+		if (result== "Success")
+		{ 
+		Notification notification= new Notification();
+		
+		try {
+			User user = dao.getUseridById(empid+"");
+			notification.setUserId(empid);
+			notification.setNotification("Leave rejected");
+			notification.setNotificationType("Leave Notification");
+			ns.createNotification(notification);	
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
 		return result;
 	}
 
